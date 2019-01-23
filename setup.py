4788533a72ba176b72f63e71b37293441e202e28
kalman.chapman <kalman.chapman@wework.com>
2019-01-23 18:05:39 -0500

    updates setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from os import path
 
 
 setup(
-    name='node2vec',  # Required
-    version='1.0',  # Required
-    description='forked node2vec for Python 3',  # Optional
-    url='https://github.com/WeConnect/node2vec',  # Optional
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    install_requires=['networkx', 'numpy', 'gensim'],  # Optional
-    package_data={  # Optional
+    name='node2vec',
+    version='1.0',
+    description='libraries for building embed-able graph walks',
+    url='https://github.com/WeConnect/node2vec',
+    package_dir={'node2vec': 'src'},
+    packages=['node2vec'],
+    install_requires=['networkx', 'numpy', 'gensim'],
+    package_data={
         '': ['*.emb', '*.edgelist'],
     }
 )
